e757dd9a60cd4a103ae305dfcd7cc3cd6c3f3810
KhiaJEO <khiajeo@gmail.com>
2022-06-01 19:12:58 +0800

    can't crack this 'id' Type Error undefined

--- src/components/MentorCard/MentorCard.jsx
+++ src/components/MentorCard/MentorCard.jsx
@@ -2,25 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // Styles
-import "./MentorCard.css"
+import "./MentorCard.css";
 
 // Components
 import InterestIcons from "../InterestIcons/InterestIcons";
 
-
 function MentorCard(props) {
-const { mentorData } = props
+  const { mentorData } = props;
 
-    return (
-        <div className="mentor-card">
-            <Link to={`/profile/${mentorData.id}`}>
-                <img className="mentorImg" src={mentorData.profile_photo} alt={`${mentorData.first_name} ${mentorData.last_name}`} />
-                <h3>{mentorData.first_name} {mentorData.last_name}</h3>
-                <h3 className="mentor-name">{mentorData.username}</h3>
-                <InterestIcons mentorData={mentorData}  />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="mentor-card">
+      <Link to={`/profile/${mentorData.id}`}>
+        <img
+          className="mentorImg"
+          src={mentorData.profile_photo}
+          alt={`${mentorData.first_name} ${mentorData.last_name}`}
+        />
+        <h3>
+          {mentorData.first_name} {mentorData.last_name}
+        </h3>
+        <h3 className="mentor-name">{mentorData.username}</h3>
+        <InterestIcons mentorData={mentorData} />
+      </Link>
+    </div>
+  );
 }
 
 export default MentorCard;
