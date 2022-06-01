06aa47462b7cee622be3fe4003af447d42a8b2df
KhiaJEO <khiajeo@gmail.com>
2022-06-01 17:44:11 +0800

    Update MentorsPage to display MentorCard

--- src/pages/MentorsPage.jsx
+++ src/pages/MentorsPage.jsx
@@ -7,45 +7,39 @@ import "./MentorsPage.css";
 import MentorCard from "../components/MentorCard/MentorCard";
 
 function MentorsPage() {
+  // State
+  const [mentorList, setMentorList] = useState([]);
+  //   Adding the [] into the useState() has displayed items on mentor page
 
+  // Actions and Helpers
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}user/`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMentorList(data);
+      });
+  }, []);
 
-    // State
-    const [mentorList, setMentorList] = useState();
+  return (
+    <div>
+      <h1>This is the Mentors Page!</h1>
 
-
-    // Actions and Helpers
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}user/`)
-            .then((results) => {
-                return results.json();
-            })
-            .then((data) => {
-                console.log(data)
-                setMentorList(data);
-            });
-    }, []);
-
-
-    return (
-        <div>
-
-            <h1>This is the Mentors Page!</h1>
-
-            <div 
-            id="mentor-list">
-                {mentorList.map((mentorData) => {
-                    console.log(mentorData);
-                    return (
-                        <MentorCard
-                            key={`mentor-${mentorData.id}`}
-                            mentorData={mentorData} 
-                        />
-                    );
-                })}
-            </div>
-            
-        </div>
-    )
+      <div id="mentor-list">
+        {mentorList.map((mentorData, key) => {
+          console.log(mentorData);
+          return (
+            <MentorCard
+              key={`mentor-${mentorData.id}`}
+              mentorData={mentorData}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default MentorsPage;
