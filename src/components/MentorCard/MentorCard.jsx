6402f55d8ab7166e5dc55dbaacce3e6f856244bf
KhiaJEO <khiajeo@gmail.com>
2022-06-02 10:57:31 +0800

    adding ? to the mentorData props to solve TypeError

--- src/components/MentorCard/MentorCard.jsx
+++ src/components/MentorCard/MentorCard.jsx
@@ -7,21 +7,22 @@ import "./MentorCard.css";
 // Components
 import InterestIcons from "../InterestIcons/InterestIcons";
 
+// adding ? to the mentorData props to solve TypeError
 function MentorCard(props) {
   const { mentorData } = props;
 
   return (
     <div className="mentor-card">
-      <Link to={`/profile/${mentorData.id}`}>
+      <Link to={`/profile/${mentorData?.id}`}>
         <img
           className="mentorImg"
-          src={mentorData.profile_photo}
-          alt={`${mentorData.first_name} ${mentorData.last_name}`}
+          src={mentorData?.profile_photo}
+          alt={`${mentorData?.first_name} ${mentorData.last_name}`}
         />
         <h3>
-          {mentorData.first_name} {mentorData.last_name}
+          {mentorData?.first_name} {mentorData?.last_name}
         </h3>
-        <h3 className="mentor-name">{mentorData.username}</h3>
+        <h3 className="mentor-name">{mentorData?.username}</h3>
         <InterestIcons mentorData={mentorData} />
       </Link>
     </div>
